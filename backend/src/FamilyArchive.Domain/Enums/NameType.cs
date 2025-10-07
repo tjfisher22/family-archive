79d5605ab9f97b530278813c3f4d212f8e9a327e
TJ Fisher <tjfisher2015@gmail.com>
2025-10-06 23:51:14 -0600

    Add enums for Name, Partnership, and Relationship types
    Added the `NameType` enum to represent various name categories,
    including `FirstName`, `MiddleName`, `LastName`, and others.
    
    Introduced the `PartnershipType` enum to define partnership
    types such as `Marriage`, `CivilUnion`, `DomesticPartnership`,
    and more.
    
    Created the `RelationshipType` enum to describe relationship
    types, including biological, step, adoptive, and non-traditional
    family structures like `OtherMother` and `OtherFather`.

---
+++ backend/src/FamilyArchive.Domain/Enums/NameType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyArchive.Domain.Enums;
+
+public enum NameType
+{
+    FirstName,
+    MiddleName,
+    LastName,
+    MaidenName,
+    ChosenName,
+    NickName,
+    DeadName,
+    LegalName,
+    OtherName,
+}
