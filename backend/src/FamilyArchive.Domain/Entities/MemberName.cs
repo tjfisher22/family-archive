4d14863bd249c25d9349560d43c9749bd6786c31
TJ Fisher <tjfisher2015@gmail.com>
2025-10-06 23:50:24 -0600

    Add MemberName entity to represent member names
    A new `MemberName` class was introduced in the `FamilyArchive.Domain.Entities` namespace. This class includes properties for storing a member's name (`Value`), its type (`NameType` enum), and visibility (`Hidden`). `Order` will be used to sort names when displayed.
    
    The `NameType` enum is referenced to categorize name types (e.g., FirstName, LastName, Nickname).

---
+++ backend/src/FamilyArchive.Domain/Entities/MemberName.cs
@@ -0,0 +1,22 @@
+﻿using FamilyArchive.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyArchive.Domain.Entities;
+
+public class MemberName
+{
+    public Guid Id { get; set; }
+    public Guid MemberId { get; set; }
+    public Member Member { get; set; } = default!;
+
+    public string Value { get; set; } = default!; // The actual name, supports Unicode by default in .NET
+    public NameType? Type { get; set; } // Enum: FirstName, MiddleName, LastName, ChosenName, Nickname, etc.
+    public int Order { get; set; } // Display order
+    public bool Hidden { get; set; } // Hide from display if true
+}
+
