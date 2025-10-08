8ff7b77fdbdf4c9dd45cee13f93351f2f8322ce4
TJ Fisher <tjfisher2015@gmail.com>
2025-10-08 13:08:10 -0600

    Initial Unit test setup

---
+++ backend/test/FamilyArchive.UnitTests/MemberNameTests.cs
@@ -0,0 +1,32 @@
+﻿using FamilyArchive.Domain.Entities;
+using FamilyArchive.Domain.Enums;
+using System;
+
+namespace FamilyArchive.UnitTests;
+
+public class MemberNameTests
+{
+    [Fact]
+    public void CanCreateMemberName_WithAllProperties()
+    {
+        var member = new Member { Id = Guid.NewGuid() };
+        var name = new MemberName
+        {
+            Id = Guid.NewGuid(),
+            Member = member,
+            MemberId = member.Id,
+            Value = "Aragorn",
+            Type = NameType.ChosenName,
+            OtherNameType = null,
+            Order = 1,
+            Hidden = false
+        };
+
+        Assert.Equal("Aragorn", name.Value);
+        Assert.Equal(NameType.ChosenName, name.Type);
+        Assert.Null(name.OtherNameType);
+        Assert.False(name.Hidden);
+        Assert.Equal(member.Id, name.MemberId);
+        Assert.Equal(member, name.Member);
+    }
+}
