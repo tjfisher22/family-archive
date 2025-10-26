03399a1cfca9eadd26a39c375fb007b949efb985
TJ Fisher <tjfisher2015@gmail.com>
2025-10-26 10:20:58 -0600

    Add ImportController with mass import endpoint

---
+++ backend/src/FamilyArchive.Api/Controllers/ImportController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyArchive.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ImportController : ControllerBase
+{
+    private readonly IMassImportService _importService;
+
+    public ImportController(IMassImportService importService)
+    {
+        _importService = importService;
+    }
+
+    [HttpPost]
+    [Route("mass")]
+    public async Task<IActionResult> MassImport([FromForm] IFormFile file)
+    {
+        var result = await _importService.ImportAsync(file);
+        return Ok(result);
+    }
+}
