1c9a640d715afc81bb2d2a9649ac75d5d6f21d06
TJ Fisher <tj.fisher@centeva.com>
2025-11-25 14:46:23 -0700

    Implement relationshipPath class

---
+++ backend/src/FamilyArchive.Domain/Services/RelationshipPath.cs
@@ -0,0 +1,38 @@
+﻿using FamilyArchive.Domain.Entities;
+using FamilyArchive.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyArchive.Domain.Services;
+
+public class RelationshipPath
+{
+    public List<RelationshipStep> Steps { get; } = new List<RelationshipStep>();
+    public void AddStep(RelationshipStep step)
+    {
+        ArgumentNullException.ThrowIfNull(step);
+        ArgumentNullException.ThrowIfNull(step.Member, nameof(step.Member));
+        
+        Steps.Add(step);
+    }
+    public void ReverseSteps()
+    {
+        Steps.Reverse();
+    }
+    public Member? CommonAncestor { get; set; } = default!; //End Node
+}
+
+public class RelationshipStep
+{
+    public Direction Direction { get; set; }
+    public Member Member { get; set; } = default!;
+    public Gender Gender => Member.Gender;
+}
+public enum Direction
+{
+    Up,
+    Down
+}
