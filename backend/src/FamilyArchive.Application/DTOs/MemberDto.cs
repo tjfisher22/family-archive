115a3edceb3d19177cc0a912dd606cd69ef02097
TJ Fisher <tj.fisher@centeva.com>
2026-01-09 16:08:14 -0700

    Refactor to use new gender enum

--- backend/src/FamilyArchive.Application/DTOs/MemberDto.cs
+++ backend/src/FamilyArchive.Application/DTOs/MemberDto.cs
@@ -1,3 +1,4 @@
+using FamilyArchive.Domain.Enums;
 using System;
 using System.Collections.Generic;
 
@@ -8,7 +9,7 @@ public class MemberDto
     public Guid Id { get; set; }
     public DateTime? BirthDate { get; set; }
     public DateTime? DeathDate { get; set; }
-    public string? Gender { get; set; }
+    public Gender Gender { get; set; }
     public Guid? FamilyId { get; set; }
     public List<MemberNameDto> Names { get; set; } = new();
 }
